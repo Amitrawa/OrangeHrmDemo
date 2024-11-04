596a29c9fdf3cb1d91fa114e9461538d76765315
91875 <91875@Sonali>
2024-11-04 23:50:51 +0530

    polo

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -46,7 +46,14 @@ public class Login {
         driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
         driver.findElement(By.xpath("//input[@name='password']"))   .sendKeys("admin123");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
+    }
+        @Test
+        public void loginWithInvalidCred() {
+        
 
+            driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
+            driver.findElement(By.xpath("//input[@name='password']"))   .sendKeys("admwwin123");
+            driver.findElement(By.xpath("//button[@type='submit']")).click();
     }
 
     @AfterMethod
